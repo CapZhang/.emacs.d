0a2600f8e458aadd7d8e5c27019a2183d884ef49
Capzhang <861682249@qq.com>
2020-11-01 09:29:45 +0800

    add the dir elpa

---
+++ elpa/lispy-20201029.1559/lispy-pkg.el
@@ -0,0 +1,11 @@
+(define-package "lispy" "20201029.1559" "vi-like Paredit"
+  '((emacs "24.3")
+    (ace-window "0.9.0")
+    (iedit "0.9.9")
+    (counsel "0.11.0")
+    (hydra "0.14.0")
+    (zoutline "0.1.0"))
+  :commit "41f5574aefb69930d9bdcbe4e0cf642005369765")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
